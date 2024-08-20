b20bb171ffa4c063b14dc5056fe828872be8461a
masman <asmanoden@gmail.com>
2024-08-20 15:01:41 +0300

    firstcommit:models

---
+++ backend/models.go
@@ -0,0 +1,13 @@
+package backend
+
+type Artists struct {
+	ID              int      `json:"id"`
+	ImageURL        string   `json:"image"`
+	Name            string   `json:"name"`
+	Members         []string `json:"members"`
+	CreationDate    int      `json:"creationDate"`
+	FirstAlbum      string   `json:"firstAlbum"`
+	LocationsURL    string   `json:"locations"`
+	ConcertDatesURL string   `json:"concertDates"`
+	RelationsURL    string   `json:"relations"`
+}
