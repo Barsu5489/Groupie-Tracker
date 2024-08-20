b4848406773798f1a4a7a35ca53b52a01f8a4c58
kada <adakennedy6@gmail.com>
2024-08-20 21:00:21 +0300

    implementing utility function to get and decode artists data, adding global variables to cache json data to prevent api calls on reloading, enhancing loading time

--- backend/utils.go
+++ backend/utils.go
@@ -11,24 +11,54 @@ var client = &http.Client{
 	Timeout: 10 * time.Second,
 }
 
+var (
+	artistsData   json.RawMessage
+	locationsData json.RawMessage
+	datesData     json.RawMessage
+	relationsData json.RawMessage
+)
+
 const APIURL = "https://groupietrackers.herokuapp.com/api"
 
-func getJSONData(url string) (json.RawMessage, error) {
-	resp, err := client.Get(url)
+func getJSONData(endpoint string) (json.RawMessage, error) {
+	resp, err := client.Get(APIURL + endpoint)
 	if err != nil {
-		return nil, fmt.Errorf("failed to get json data: %w", err)
+		return nil, fmt.Errorf("failed to get %s json data: %w", endpoint, err)
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("received a non-200 response code: %d", resp.StatusCode)
+		return nil, fmt.Errorf("received a non-200 response code for %s: %d", endpoint, resp.StatusCode)
 	}
 
 	var jsonString json.RawMessage
 	err = json.NewDecoder(resp.Body).Decode(&jsonString)
 	if err != nil {
-		return nil, fmt.Errorf("failed to decode json data: %w", err)
+		return nil, fmt.Errorf("failed to decode %s json data: %w", endpoint, err)
 	}
 
 	return jsonString, nil
+}
+
+func GetAndUnmarshalArtists() ([]Artists, error) {
+	artists := []Artists{}
+	if artistsData != nil {
+		err := json.Unmarshal(artistsData, &artists)
+		if err != nil {
+			return artists, err
+		}
+		return artists, nil
+	}
+
+	jsonData, err := getJSONData("/artists")
+	if err != nil {
+		return artists, err
+	}
+
+	err = json.Unmarshal(jsonData, &artists)
+	if err != nil {
+		return artists, err
+	}
+
+	return artists, nil
 }
