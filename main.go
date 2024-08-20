3fb8f218de11287a82fc9ea547327aee8bc0264d
kada <adakennedy6@gmail.com>
2024-08-20 19:48:09 +0300

    implementing the entry main function to start a server for the project listening at port 8080

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"groupie-tracker/backend"
+)
+
+func main() {
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: backend.RegisterRoutes(),
+	}
+	log.Println("server listening on http://localhost:8080")
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatalf("failed to start server: %v", err)
+	}
+}
