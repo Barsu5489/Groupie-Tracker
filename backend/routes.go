2e06f9160f9fdf9bb8f2ec884b9db5b4bc588bfe
kada <adakennedy6@gmail.com>
2024-08-21 10:42:31 +0300

    implementing a new multiplexer for registering routes and assigning handlers to the routes

---
+++ backend/routes.go
@@ -0,0 +1,15 @@
+package backend
+
+import (
+	"net/http"
+)
+
+func RegisterRoutes() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("frontend"))))
+	mux.HandleFunc("/", artistsHandler)
+	mux.HandleFunc("/locations", locationsHandler)
+	mux.HandleFunc("/dates", datesHandler)
+	mux.HandleFunc("/relation", artistDetailsHandler)
+	return mux
+}
